7ac29f854c8b936f68cca5b419f237be43dcbc3b
Davidmorelo28 <dsuarez2802@gmail.com>
2026-05-27 21:46:48 +0000

    proyecto completo

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import HidroBot from './HidroBot'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <HidroBot />
+  </React.StrictMode>,
+)
